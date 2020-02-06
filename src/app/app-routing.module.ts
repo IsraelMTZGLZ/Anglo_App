2ef430404010489f0e2ad49d40817eb4d2e5d402
IsraelMTZGLZ <israelmg250598@gmail.com>
2020-02-06 13:33:46 -0600

    hola

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {
+    path: 'login',
+    loadChildren: () => import('./public/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'home_user',
+    loadChildren: () => import('./public/home/home.module').then( m => m.HomePageModule)
+  },
 ];
 
 @NgModule({
